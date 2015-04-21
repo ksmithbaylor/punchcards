1a4ea6b5fb51e00e6e1802b4b9532f190ce67f7f
Kevin Smith <ksmithbaylor@gmail.com>
2015-04-21 17:27:44 -0400

    add router middleware

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var http = require('http');
+var final = require('finalhandler');
+var router = require('router')();
+
+router.get('/', function (req, res) {
+  res.end('hello world\n');
+});
 
 var server = http.createServer(function (req, res) {
-  res.end('hello world\n');
+  router(req, res, final(req, res));
 });
 
 server.listen(5555);
