9d852ab8d2e9e5c1da53d4308e7cf900f80f900a
Kevin Smith <ksmithbaylor@gmail.com>
2015-04-22 09:52:50 -0400

    simple request to github api with auth

--- punchcard.js
+++ punchcard.js
@@ -1,7 +1,15 @@
 var _ = require('lodash');
 var co = require('co');
+var request = require('co-request');
+
+var options = {
+  headers: {
+    'User-Agent': 'user-punchcards',
+    'Authorization': 'token ' + process.env['TOKEN']
+  }
+};
 
 module.exports = co.wrap(function*(username, cb) {
-  var result = 'hello world';
-  return JSON.stringify(result);
+  var result = yield request('https://api.github.com/zen', options);
+  return username + ' says ' + result.body;
 });
