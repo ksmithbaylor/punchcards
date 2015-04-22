851ab4fdba7b9ed51c2a06cf3ca07a7e6563a6fb
Kevin Smith <ksmithbaylor@gmail.com>
2015-04-21 23:33:04 -0400

    add punchcard function and send json

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ var http = require('http');
 var fs = require('fs');
 var final = require('finalhandler');
 var router = require('router')();
+var punchcard = require('./punchcard');
 
 router.get('/', function (req, res) {
   fs.createReadStream('index.html').pipe(res);
 });
 
 router.get('/users/:user/', function (req, res) {
-  res.end(req.params.user + '\n');
+  res.end(JSON.stringify(punchcard(req.params.user)));
 });
 
 var server = http.createServer(function (req, res) {
