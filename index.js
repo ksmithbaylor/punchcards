e7eaafa1304474732852c3eb1708bb29d2f56ee9
Kevin Smith <ksmithbaylor@gmail.com>
2015-04-21 23:25:39 -0400

    serve static file and respond to api url

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var http = require('http');
+var fs = require('fs');
 var final = require('finalhandler');
 var router = require('router')();
 
 router.get('/', function (req, res) {
-  res.end('hello world\n');
+  fs.createReadStream('index.html').pipe(res);
+});
+
+router.get('/users/:user/', function (req, res) {
+  res.end(req.params.user + '\n');
 });
 
 var server = http.createServer(function (req, res) {
