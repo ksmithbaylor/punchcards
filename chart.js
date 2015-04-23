44abbdb252d5f311e1470db64becd509f7cbb825
Kevin Smith <ksmithbaylor@gmail.com>
2015-04-23 08:56:01 -0400

    get json and also fake json

--- chart.js
+++ chart.js
@@ -1,7 +1,18 @@
 var userField = document.getElementById('userField');
 var submitButton = document.getElementById('submitButton');
+var fake = document.getElementById('fake');
+
+fake.addEventListener('click', function () {
+  var url = '/fake';
+  d3.json(url, handleResponse);
+});
 
 submitButton.addEventListener('click', function () {
-  var username = userField.value;
-  console.log(username);
+  var url = '/users/' + userField.value;
+  d3.json(url, handleResponse);
 });
+
+function handleResponse(err, data) {
+  if (err) throw err;
+  console.log(data);
+}
--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function static(path, file, type) {
 static('/', 'index.html', 'text/html');
 static('/d3.min.js', 'node_modules/d3/d3.min.js', 'text/javascript');
 static('/chart.js', 'chart.js', 'text/javascript');
+static('/fake', 'ksmithbaylor', 'text/json');
 
 router.get('/users/:user/', (req, res) => {
   punchcard(req.params.user).then(result => res.end(result));
