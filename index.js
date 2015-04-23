4952f1f2de3f82239423856e06b75cce1bc48bb1
Kevin Smith <ksmithbaylor@gmail.com>
2015-04-22 22:30:00 -0400

    include d3 on the page

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ var final = require('finalhandler');
 var router = require('router')();
 var punchcard = require('./punchcard');
 
-router.get('/', (req, res) => {
-  fs.createReadStream('index.html').pipe(res);
-});
+function static(path, file, type) {
+  router.get(path, (req, res) => {
+    res.writeHead(200, {'Content-Type': type});
+    fs.createReadStream(file).pipe(res);
+  });
+}
+
+static('/', 'index.html', 'text/html');
+static('/d3.min.js', 'node_modules/d3/d3.min.js', 'text/javascript');
 
 router.get('/users/:user/', (req, res) => {
   punchcard(req.params.user).then(result => res.end(result));
