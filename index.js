d5b397d0d178101d1e75f458f1f8508650d1e6a7
Kevin Smith <ksmithbaylor@gmail.com>
2015-04-22 22:07:50 -0400

    clean up formatting

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ var final = require('finalhandler');
 var router = require('router')();
 var punchcard = require('./punchcard');
 
-router.get('/', (req, res) =>
-  fs.createReadStream('index.html').pipe(res));
+router.get('/', (req, res) => {
+  fs.createReadStream('index.html').pipe(res);
+});
 
-router.get('/users/:user/', (req, res) =>
-  punchcard(req.params.user).then(result => res.end(result)));
+router.get('/users/:user/', (req, res) => {
+  punchcard(req.params.user).then(result => res.end(result));
+});
 
-http.createServer((req, res) =>
-  router(req, res, final(req, res))).listen(5555);
+http.createServer((req, res) => {
+  router(req, res, final(req, res));
+}).listen(5555);
