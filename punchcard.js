014972341ec9b6685c2e9068ca3da59969891e37
Kevin Smith <ksmithbaylor@gmail.com>
2015-04-22 22:08:39 -0400

    get stats across all repos

--- punchcard.js
+++ punchcard.js
@@ -11,8 +11,31 @@ var options = {
 
 var base = 'https://api.github.com';
 
+function* githubJSON(path) {
+  var response = yield request(base + path, options);
+  return JSON.parse(response.body);
+}
+
 module.exports = co.wrap(function*(username) {
-  var reposUrl = base + '/users/' + username + '/repos';
-  var result = yield request(reposUrl, options);
-  return username + ' says ' + result.body;
+  function* usernameToReposPromise(username) {
+    var url = '/users/' + username + '/repos';
+    return yield githubJSON(url);
+  }
+
+  function nameToPunchcard(name) {
+    var url = '/repos/' + username + '/' + name + '/stats/punch_card';
+    return githubJSON(url);
+  }
+
+  function squashTimes(times) {
+    return _.reduce(times, (a, b) => [a[0], a[1], a[2] + b[2]]);
+  }
+
+  var repos = yield usernameToReposPromise(username);
+  var names = _.pluck(repos, 'name');
+  var stats = yield _.map(names, nameToPunchcard);
+  var byTime = _.zip.apply(null, stats);
+  var rolledUp = _.map(byTime, squashTimes);
+
+  return JSON.stringify(rolledUp);
 });
